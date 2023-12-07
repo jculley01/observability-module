f579f603b07696f60a58b9146f3a9528edb2245f
jculley01 <113144839+jculley01@users.noreply.github.com>
2023-12-06 20:18:12 -0500

    updated files

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jculley01/observability-module
+
+go 1.21.2
+
+require github.com/google/uuid v1.4.0
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
--- registration/registration.go
+++ registration/registration.go
@@ -1,1 +1,65 @@
 package registration
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"time"
+)
+
+type Registration struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+// RegisterService handles the registration of the service.
+func RegisterService(webSocketURL, serviceID, serviceType string) error {
+	registrationData := Registration{
+		Name: serviceID,
+		Type: serviceType,
+	}
+
+	jsonData, err := json.Marshal(registrationData)
+	if err != nil {
+		return fmt.Errorf("error marshalling registration data: %w", err)
+	}
+
+	err = registerWithRegistry(webSocketURL, jsonData)
+	if err != nil {
+		return fmt.Errorf("registration failed: %w", err)
+	}
+
+	return nil
+}
+
+func registerWithRegistry(registryURL string, jsonData []byte) error {
+	ticker := time.NewTicker(30 * time.Second) // Retry every 30 seconds
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			c, _, err := websocket.DefaultDialer.Dial(registryURL, nil)
+			if err != nil {
+				fmt.Println("Error connecting to WebSocket, retrying...:", err)
+				continue
+			}
+
+			err = c.WriteMessage(websocket.TextMessage, jsonData)
+			if err != nil {
+				fmt.Println("Error sending registration data, retrying...:", err)
+				c.Close()
+				continue
+			}
+
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				fmt.Println("Error reading response, retrying...:", err)
+			} else {
+				fmt.Printf("Response from server: %s\n", message)
+			}
+			c.Close()
+			return nil
+		}
+	}
+}
